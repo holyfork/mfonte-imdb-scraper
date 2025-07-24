b36b7a994c508b2f41a3321921f8095d371e30da
Milan Felix Šulc <rkfelix@gmail.com>
2025-07-24 12:06:46 +0200

    Parser: fix null hero container

--- src/Parser.php
+++ src/Parser.php
@@ -439,11 +439,13 @@ class Parser
         $heroContainer = self::getHeroContainer($dom);
         if ($heroContainer) {
             $interestsContainer = $heroContainer->findOneOrFalse('div[data-testid="interests"]');
-            $genres = $interestsContainer->find('a.ipc-chip span');
-            if (count((array) $genres) > 0) {
-                return array_map(function ($genre) {
-                    return self::clean($genre->innerText());
-                }, (array) $genres);
+            if ($interestsContainer !== false) {
+                $genres = $interestsContainer->find('a.ipc-chip span');
+                if (count((array) $genres) > 0) {
+                    return array_map(function ($genre) {
+                        return self::clean($genre->innerText());
+                    }, (array) $genres);
+                }
             }
         }
 
@@ -642,7 +644,7 @@ class Parser
                 // if there is no collection element, skip this section
                 continue;
             }
-            
+
             $role = self::clean($collectionElement->innerText());
 
             if (str_ireplace([
@@ -722,7 +724,7 @@ class Parser
                         'character' => $character,
                     ]
                 ];
-                
+
                 $credits[] = $credit;
             }
         }
